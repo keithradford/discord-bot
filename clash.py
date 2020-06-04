77c961f31fbb63dd93458d37893d419702d7e006
keith <khr877@gmail.com>
2020-06-04 15:23:05 -0700

    Made code pretty

--- clash.py
+++ clash.py
@@ -13,8 +13,7 @@ headers = {
     'authorization': 'Bearer ' + os.environ.get('CLASH_KEY')
 }
 
-# Clash of Clans API requires IP so use Fixie as a proxy to
-# make requests to the API while the bot is deployed on Heroku.
+# Clash of Clans API requires IP, so use Fixie as a proxy to make requests to the API while the bot is deployed on Heroku.
 proxyDict = {
     "http"  : os.environ.get('FIXIE_URL', ''),
     "https" : os.environ.get('FIXIE_URL', '')
@@ -24,9 +23,12 @@ class Clan:
     '''
     Clan class offers methods to fetch data related to the clan.
     '''
+
     url = 'https://api.clashofclans.com/v1/clans/%23QOUUCYV2/'
 
     def get_war(self):
+        '''Returns data related to the clan's current war.'''
+
         response = requests.get(self.url + 'currentwar', headers=headers, proxies=proxyDict)
         data = response.json()
         to_ret = ''
@@ -34,26 +36,32 @@ class Clan:
             to_ret = 'The DMND Life is not in a war right now.'
         else:
             to_ret += '```\nThe DMND Life is in a war vs. ' + data['opponent']['name'] + '.\n\n' + 'The DMND Life: ' + data['clan']['stars'] + ' stars, ' + data['clan']['attacks'] + 'attacks\n' + data['opponent']['name'] + ': ' + data['opponent']['stars'] + ' stars, ' + data['opponent']['attacks'] + 'attacks\n```'
+
         return to_ret
 
     def get_members(self):
         '''Returns a list of members in the clan.'''
+
         response = requests.get(self.url + 'members', headers=headers, proxies=proxyDict)
         data = response.json()
         to_ret = ''
         for user in data['items']:
            to_ret += '\n' + ['name']
+
         return to_ret
 
     def get_random_member(self):
         '''Returns the name of a random member in the clan'''
+
         response = requests.get(self.url + 'members', headers=headers, proxies=proxyDict)
         data = response.json()
         member = random.choice(data['items'])
+
         return member['name'] + ' is moist.'
 
     def cwl_info(self):
         '''Returns info related to the Clan War League'''
+
         response = requests.get(self.url + 'currentwar/leaguegroup', headers=headers, proxies=proxyDict)
         data = response.json()
         if data['state'] == 'inWar':
@@ -63,10 +71,12 @@ class Clan:
             to_ret += '\n -' + clan['name']
 
         to_ret += '\n```'
+
         return to_ret
 
     def cwl_clan_info(self, name):
         '''Returns info related to a clan in tbe Clan War League'''
+
         name = name.lower()
         response = requests.get(self.url + 'currentwar/leaguegroup', headers=headers, proxies=proxyDict)
         data = response.json()
@@ -77,16 +87,21 @@ class Clan:
                 for member in clan['members']:
                     to_ret += '\n -' + member['name'] + ': ' 'TH' + str(member['townHallLevel'])
                 to_ret += '\n```'
+
                 return to_ret
+
         return 'Clan not in CWL'
 
     def get_member(self, name):
         '''Returns info related to a member in the clan'''
+
         name = name.lower()
         response = requests.get(self.url + 'members', headers=headers, proxies=proxyDict)
         data = response.json()
         for user in data['items']:
             if name == user['name'].lower():
                 to_ret = user['name'] + '\n' + user['role'] + '\n' + 'Trophies: ' + str(user['trophies']) + ',  Donated: ' + str(user['donations']) + ', Received: ' + str(user['donationsReceived'])
+
                 return to_ret
+
         return 'User not found in clan.'
