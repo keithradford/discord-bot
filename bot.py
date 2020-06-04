454d929e23058e30689923bfb1145b47129bd3fb
keith <khr877@gmail.com>
2020-06-04 15:15:58 -0700

    Added functionality for command args with spaces

--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
-# bot.py
+'''
+bot.py
+Runs the Discord bot.
+Creates the commands and their actions that users can call the bot upon.
+'''
+
 import os
 import discord
 from discord.ext import commands
@@ -17,12 +22,13 @@ async def on_ready():
 async def get_cwl(ctx, *args):
     c = clash.Clan()
     message = c.cwl_info()
-    if len(args) == 1:
-        message = c.cwl_clan_info(args[0])
+    s = ' '
+    if len(args) >= 1:
+        message = c.cwl_clan_info(s.join(args))
     await ctx.send(message)
 
 @bot.command(name='moist', help='Picks the moist member of the day.')
-async def get_cwl(ctx, *args):
+async def get_cwl(ctx):
     c = clash.Clan()
     message = c.get_random_member()
     await ctx.send(message)
@@ -33,6 +39,12 @@ async def get_member(ctx, name):
     message = c.get_member(name)
     await ctx.send(message)
 
+@bot.command(name='war', help="Fetches the clan's war state.")
+async def get_war(ctx):
+    c = clash.Clan()
+    message = c.get_war()
+    await ctx.send(message)
+
 @bot.command(name='roll_dice', help='Simulates rolling dice.')
 async def roll(ctx, number_of_dice:int, number_of_sides:int):
     dice = [
@@ -40,5 +52,6 @@ async def roll(ctx, number_of_dice:int, number_of_sides:int):
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+
 
 bot.run(TOKEN)
--- clash.py
+++ clash.py
@@ -1,3 +1,9 @@
+'''
+clash.py
+Performs HTTP requests to the Clash of Clans API to fetch clan and user data.
+Formats data into strings to be sent as messages by the Discord bot.
+'''
+
 import requests
 import random
 import os
@@ -7,30 +13,49 @@ headers = {
     'authorization': 'Bearer ' + os.environ.get('CLASH_KEY')
 }
 
+# Clash of Clans API requires IP so use Fixie as a proxy to
+# make requests to the API while the bot is deployed on Heroku.
 proxyDict = {
     "http"  : os.environ.get('FIXIE_URL', ''),
     "https" : os.environ.get('FIXIE_URL', '')
 }
 
 class Clan:
+    '''
+    Clan class offers methods to fetch data related to the clan.
+    '''
     url = 'https://api.clashofclans.com/v1/clans/%23QOUUCYV2/'
 
+    def get_war(self):
+        response = requests.get(self.url + 'currentwar', headers=headers, proxies=proxyDict)
+        data = response.json()
+        to_ret = ''
+        if data['state'] == 'notInWar':
+            to_ret = 'The DMND Life is not in a war right now.'
+        else:
+            to_ret += '```\nThe DMND Life is in a war vs. ' + data['opponent']['name'] + '.\n\n' + 'The DMND Life: ' + data['clan']['stars'] + ' stars, ' + data['clan']['attacks'] + 'attacks\n' + data['opponent']['name'] + ': ' + data['opponent']['stars'] + ' stars, ' + data['opponent']['attacks'] + 'attacks\n```'
+        return to_ret
+
     def get_members(self):
+        '''Returns a list of members in the clan.'''
         response = requests.get(self.url + 'members', headers=headers, proxies=proxyDict)
         data = response.json()
+        to_ret = ''
         for user in data['items']:
-           return user['name']
+           to_ret += '\n' + ['name']
+        return to_ret
 
     def get_random_member(self):
+        '''Returns the name of a random member in the clan'''
         response = requests.get(self.url + 'members', headers=headers, proxies=proxyDict)
         data = response.json()
         member = random.choice(data['items'])
         return member['name'] + ' is moist.'
 
     def cwl_info(self):
+        '''Returns info related to the Clan War League'''
         response = requests.get(self.url + 'currentwar/leaguegroup', headers=headers, proxies=proxyDict)
         data = response.json()
-        print(data)
         if data['state'] == 'inWar':
             state = 'In War\n'
         to_ret = '```\n' + state + 'Season: ' + str(data['season']) + '\n\n' + 'Clans: '
@@ -41,6 +66,7 @@ class Clan:
         return to_ret
 
     def cwl_clan_info(self, name):
+        '''Returns info related to a clan in tbe Clan War League'''
         response = requests.get(self.url + 'currentwar/leaguegroup', headers=headers, proxies=proxyDict)
         data = response.json()
         to_ret = '```\n'
@@ -54,6 +80,7 @@ class Clan:
         return 'Clan not in CWL'
 
     def get_member(self, name):
+        '''Returns info related to a member in the clan'''
         response = requests.get(self.url + 'members', headers=headers, proxies=proxyDict)
         data = response.json()
         for user in data['items']:
